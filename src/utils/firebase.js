e79d11bcd3e1dd28a854294497e6b0a86e8718fa
Stone Visibee <visibleone.stone@gmail.com>
2023-09-15 14:08:39 +0630

    fix bugs

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,6 +2,9 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
+import axios from 'axios';
+import "https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js";
+import "https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -20,7 +23,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const messaging = getMessaging(app);
 
 export const requestPermission = () => {
@@ -39,6 +41,7 @@ export const requestPermission = () => {
                         console.log('Client Token: ', currentToken);
 
                         //    messaging.getInstance().subscribeToTopic("news");
+                        callChannel(currentToken);
                     } else {
 
                         console.log('Failed to generate the app registration token.');
@@ -56,8 +59,6 @@ export const requestPermission = () => {
 
 }
 
-requestPermission();
-
 export const onMessageListener = () =>
     new Promise((resolve) => {
         onMessage(messaging, (payload) => {
@@ -69,4 +70,20 @@ function subscribeToTopic() {
     // test()
     //     .subscribeToTopic('vo-channel')
     //     .then(() => console.log('Subscribed to topic!'));
+}
+
+
+
+function callChannel(token) {
+    let authToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiYjVlMjlmYWRhN2Q1OThmYzc4ZDQ3ZDBjYWM0ZGQ0ZTQ0YzM4ZDM3MWQyZmYxNmEyNWUzNThlMGE0NWM5ZWM5ZmRiZjhkMTIwOThlYjk1YzUiLCJpYXQiOjE2OTQ3NjMwOTkuOTUxMzI5OTQ2NTE3OTQ0MzM1OTM3NSwibmJmIjoxNjk0NzYzMDk5Ljk1MTMzNTkwNjk4MjQyMTg3NSwiZXhwIjoxNjk1MTk1MDk5Ljk0MjYxMjg4NjQyODgzMzAwNzgxMjUsInN1YiI6IjQiLCJzY29wZXMiOltdfQ.FEnntKmZSnIPqjM4X0nbsSb7sz3oHkdmIO7zUqBeKFDhNPnI--HR7yY1UpMJSXzMUQlYvilWK03CgnV0j9Zv2z4UPf-42YfsL5RUsthLgXhqZ0feOjmqJ8vgbCy8I6EC9PJh5X2kM8aruq52hV9GiJ_xEe9ibwO42rGqYZuNqV-q4kkt-aMoDjDd_HBF2yuQZyoJnt5qFQbahOGzvXnhOJwdZ-liZ_pvrPePrxza3WBSU8-ch979dNIIXfwDODfNdwrxTMMbd52jkxJxqwbCXRQ1Rl2EcoYeJGwMp3gmeqoH84qky4kW93VPunT5BeLle4B37mD2m1uKEiemUWBv0DDEjrZgLQMHw-6UOiRwzjOiS4Li9s-Iz9UPjXjuPX-z5uKZrJ9YG69EXSSu5YELsNRSBA7Ut5AlZZf9sWIsC30ZucbQ5m0MgL0O62uYWV9dF6OfgbhsQxLhz3Fu_O4YToPzzGClt-K73Ejhx8fdNy8v7wdsoKngBFqlki1ZbPSmxwftp2pPIWJYbU_yxQYlcLJqqlVE0DRi166yIQHbzd7jYAwuC1xqMzq0ebTkZb_fzdBFzV6ugzOh9GRFLgsvPSfcpYaW40vl5p9o-aWqaKrVQ9-TTgknyFyXFRxuId_MYx2zV6iagD1KulrJbfr5nbxcqVMZR0NWxuzHjRu6ojU";
+    axios.post(`https://vorpapidev.visibleone.io/api/v1/fcm-token`, {
+        token: token
+    }, {
+        headers: {
+            'Authorization': `Bearer ${authToken}`
+        }
+    }
+    ).then((result) => {
+        console.log("result ", result)
+    });
 }
